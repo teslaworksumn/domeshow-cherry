185bbd57ad57c99b030e4f9cb697f627a724b0db
Ryan Fredlund <rfredlund13@gmail.com>
2016-03-30 16:57:18 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='cherry',
+      version='0.1',
+      description='The Doooooooooooooooooooooooooooooooooome Controller',
+      author='Tesla Works',
+      packages=['cherry'],
+     )
