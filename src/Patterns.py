9d4cb22a6ac8d0ee4e7f11b849e9745b040ade71
Ryan Fredlund <rfredlund13@gmail.com>
2016-03-31 21:54:01 -0500

    Made patterns loop continuously until off(), solid(), or shutdown() is called

--- src/Patterns.py
+++ src/Patterns.py
@@ -11,41 +11,50 @@ from src.patterns.FullRandom import FullRandom
 class Patterns:
 
     # tickrate is in ms
-    def __init__(self, device, numch, tickrate):
+    def __init__(self, device, numch, tickrate, framesperpattern=32):
         self._numchannels = numch
+        self._framesperpattern = framesperpattern
         self._tickrate = tickrate
         self._output = Output(device)
+        self._islooping = False
         self._stopstream = rxsubject.Subject()
-        self.patterns = [FullRandom(numch), TargetPulse(True), TargetPulse(False),
+        self.patterns = [FullRandom(numch),
+                         TargetPulse(True),
+                         TargetPulse(False),
                          self._rotatable_horizontal_wave, self._rotatable_horizontal_wave_continuous,
                          self._beach_ball_of_death, self._counter_rotating_circles, self._solid_fade,
                          self._full_random_fade, self._spiral, self._sets_of_5]
 
     # Starts running patterns
     def start(self):
+        self._islooping = True
         self._next()
 
     # Stops all observables
     def shutdown(self):
+        self._islooping = False
         self._stopstream.on_next(None)
 
     # Make the dome a solid color
     # Because the boards persistently hold their color, only send one set of data out
     def solid(self, r, g, b):
+        self._islooping = False
         self._stopstream.on_next(None)
         data = [r, g, b] * self._numchannels
         self._output.send(data)
 
     # Get the next pattern to play, and start it
     def _next(self):
-        self._stopstream.on_next(None)
-        index = random.randint(0, len(self.patterns)-1)
-        # TODO remove (testing)
-        index = 0
-        self._tickstream = Observable.interval(self._tickrate)\
-            .take_until(self._stopstream)\
-            .map(lambda a,b: self.patterns[index].getframe(a))\
-            .subscribe(self._output.send)
+        if self._islooping:
+            self._stopstream.on_next(None)
+            index = random.randint(0, len(self.patterns)-1)
+            # TODO remove (testing)
+            index = 0
+            Observable.interval(self._tickrate)\
+                .take_until(self._stopstream)\
+                .take(self._framesperpattern)\
+                .map(lambda a,b: self.patterns[index].getframe(a))\
+                .subscribe(self._output.send, lambda e: print(e), lambda: self._next())
 
     def _rotatable_horizontal_wave(self, frame):
         pass
