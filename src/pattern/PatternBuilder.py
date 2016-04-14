0782a9b4ff45645ea3d91bdb0621557b198fe73a
Ramith Jayatilleka <ramith.jayatilleka@gmail.com>
2016-04-14 16:49:37 -0500

    build5 and build3

--- src/pattern/PatternBuilder.py
+++ src/pattern/PatternBuilder.py
@@ -11,12 +11,14 @@ def random_color():
 # Construct the data array from the patterns for each layer (repeated)
 # Layers are lists, multiples of 3, that contain channel rgb info
 def build5(layers):
-    data = \
-        _build_layer(layers[0], 10) + \
-        _build_layer(layers[1], 10) + \
-        _build_layer(layers[2], 10) + \
-        _build_layer(layers[3], 5) + \
-        _build_layer(layers[4], 5)
+    data = [0] * 40
+
+    _build_layer(data, 0, layers[0], 10)
+    _build_layer(data, 30, layers[1], 10)
+    _build_layer(data, 60, layers[2], 10)
+    _build_layer(data, 90, layers[3], 5)
+    _build_layer(data, 105, layers[4], 5)
+
     return data
 
 def build3(layers):
@@ -24,24 +26,24 @@ def build3(layers):
     # middle follows the pattern layer2 layer2 layer3
     # This is because there are 10 layer2 triangles, but only 5 layer3 triangles
     # top is simply layer4
-    bottom = _build_layer(layers[0], 20)
-    middle = _build_layer(layers[1], 15)
-    top = _build_layer(layers[2], 5)
 
-    layer0 = [0] * 30
-    layer1 = [0] * 30
-    layer2 = [0] * 30
-    layer3 = [0] * 15
-    layer4 = top
+    buf = [0] * 120
+    data = [0] * 120
 
-    for i in range(6):
-        layer2[i::6] = middle[i::9]
-    for i in range(3):
-        layer0[i::3] = bottom[i::6]
-        layer1[i::3] = bottom[i + 3::6]
-        layer3[i::3] = middle[i + 6::9]
+    _build_layer(buf, 0, layers[0], 20)
+    _build_layer(buf, 60, layers[1], 15)
+    _build_layer(data, 105, layers[2], 5)
 
-    return layer0 + layer1 + layer2 + layer3 + layer4
+    for i in range(3): # layer 0
+        data[i:30:3] = data[i:60:6]
+    for i in range(3): # layer 1
+        data[i+30:60:3] = data[i+3:60:6]
+    for i in range(6): # layer 2
+        data[i+60:90:6] = data[i+60:105:9]
+    for i in range(3): # layer 3
+        data[i+90:105:3] = data[i+66:105:9]
+
+    return data
 
 
 def build10(layers, offset):
@@ -58,20 +60,19 @@ def shift(offset, layer):
     return layer[offset:] + layer[:offset]
 
 # Helper function to create a layer's data output
+# data is the array to modify and offset is how far to start writing
 # layer is a list of rgb values; must be multiple of 3
 # triangle_count is the number of panels/triangles in this layer
-def _build_layer(layer, triangle_count):
+def _build_layer(data, offset, layer, triangle_count):
     if len(layer) % 3 != 0:
         raise ValueError( \
             'Expected layer length divisible by 3, was ' + str(len(layer)))
 
     colors = len(layer) // 3  # rgb colors in layer
-    data = []
 
     for i in range(triangle_count):
-        index = (i % colors) * 3
-        data += layer[index:index + 3]
-
-    return data
+        l_index = (i % colors) * 3
+        d_index = offset + (i * 3)
+        data[d_index:d_index + 3] = layer[l_index:l_index + 3]
 
 
