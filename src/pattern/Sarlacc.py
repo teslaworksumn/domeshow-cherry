e88709e8734ebb6145271834a66a9a4771b81407
Ramith Jayatilleka <ramith.jayatilleka@gmail.com>
2016-04-19 14:40:11 -0500

    sarlacc and tsunami

--- src/pattern/Sarlacc.py
+++ src/pattern/Sarlacc.py
@@ -2,13 +2,17 @@ import src.pattern.PatternBuilder as PB
 import random
 from rx import Observable
 
+last = [-1, -1, -1]
+
 def get_observable(color=None, offset=0, tick_period_ms=0):
     if color is None: color = PB.random_color()
     if tick_period_ms <= 0: tick_period_ms = 200
 
-    last = [-1, -1, -1]
+    layer_sets = [[last] * 10 for i in range(10)]
 
-    layer_sets = [(i * [last]) + [color] + ((9-i) * [last]) for i in range(10)]
+    for i in range(10):
+        layer_sets[i][i] = color
+
     frames = [PB.build10(layers, offset) for layers in layer_sets]
 
     return Observable.interval(tick_period_ms) \
