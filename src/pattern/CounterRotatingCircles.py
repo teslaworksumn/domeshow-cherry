8461598812efda71ddd45165482c2ab99c431f9f
Ramith Jayatilleka <ramith.jayatilleka@gmail.com>
2016-04-19 15:21:56 -0500

    Cirlces

--- src/pattern/CounterRotatingCircles.py
+++ src/pattern/CounterRotatingCircles.py
@@ -2,21 +2,21 @@ import src.pattern.PatternBuilder as PB
 from rx import Observable
 import random
 
-
-def _set_colors(colors):
-    if colors is None or len(colors) < 5:
+def _safe_colors(colors):
+    if colors is None or len(colors) < 15:
         return [c for i in range(5) for c in PB.random_color()]
     return colors
 
 def get_observable(colorsA=None, colorsB=None, tall=None, tick_period_ms=0):
-    colors = [_set_colors(colorsA), _set_colors(colorsB)]
+    colors = [_safe_colors(colorsA), _safe_colors(colorsB)]
     if tall is None: tall = (random.randint(0, 1) == 1)
     if tick_period_ms <= 0: tick_period_ms = 200
 
     frames = [None] * 10
+    
     for i in range(10):
         # Mutated, so shift 1 and -1 from last shift (cumulative shift)
-        shifted = [PB.shift(1, colors[0]), PB.shift(-1, colors[1])]
+        shifted = [PB.shift(colors[0], 0, 5, 1), PB.shift(colors[1], 0, 5, 4)]
         if tall:
             colors = [shifted[i % 2] for i in range(5)]
             frames[i] = PB.build5(colors)
