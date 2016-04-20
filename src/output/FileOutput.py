401c9fe1abc0ecdcfc91b35112bc8cc14fd418ec
Ryan Fredlund <rfredlund13@gmail.com>
2016-04-20 00:21:49 -0500

    Moved outputters to module and removed said module from .gitignore

---
+++ src/output/FileOutput.py
@@ -0,0 +1,19 @@
+
+
+class FileOutput:
+    def __init__(self, filename):
+        self._filename = filename
+        self._f = open(filename, 'a', 1)
+
+    def close(self, message):
+        print('Closing FileOutput {0}: {1}'.format(self._filename, message))
+        self._f.close()
+
+    # Data is a dictionary of length 9 * 16 = 144
+    # where each element is the 8-bit value for that channel
+    def send(self, data):
+        output = ''
+        for d in data:
+            output += str(d) + ', '
+
+        self._f.write(output + '\n')
